ee999bedfe777d8f8bee813ca4d1ec19920247b1
HumayyunKhan <humayyunatwork@gmail.com>
2025-11-10 17:05:15 +0500

    Added try catch block in whatsapp API

--- src/services/whatsapp.api.js
+++ src/services/whatsapp.api.js
@@ -1,16 +1,41 @@
 import { axiosInstance } from "./dashboard.api";
 
 export async function fetchUserProfile() {
+  try {
     const response = await axiosInstance.get(`/admin`);
     return response.data;
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    throw error;
+  }
 }
 
 export const fetchReplies = async (page = 1, limit = 10) => {
-  const { data } = await axiosInstance.get(`/whatsapp/replies?page=${page}&limit=${limit}`);
-  return data;
+  try {
+    const { data } = await axiosInstance.get(`/whatsapp/replies?page=${page}&limit=${limit}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching replies:", error);
+    throw error;
+  }
 };
 
 export const sendMessage = async (payload) => {
-  const { data } = await axiosInstance.post(`/whatsapp/send`, payload);
-  return data;
+  try {
+    const { data } = await axiosInstance.post(`/whatsapp/send`, payload);
+    return data;
+  } catch (error) {
+    console.error("Error sending message:", error);
+    throw error;
+  }
+};
+
+export const deleteReply = async (id) => {
+  try {
+    const { data } = await axiosInstance.deleteMethod(`/whatsapp/replies/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error sending message:", error);
+    throw error;
+  }
 };

a3e619b091c30692ae095d6711c15d28636816eb
HumayyunKhan <humayyunatwork@gmail.com>
2025-11-10 17:05:02 +0500

    Added try catch block in contact API

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,36 +1,57 @@
 import { axiosInstance } from "./dashboard.api";
 
-
 export async function deleteContacts(id) {
-  const response = await axiosInstance.deleteMethod(`contacts/${id}`);
-  return response.data;
+  try {
+    const response = await axiosInstance.deleteMethod(`contacts/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    throw error;
+  }
 }
 
-export async function fetchContacts(page=1, perPage=50) {
-  const params = {
-    page: page.toString(),
-    perPage: perPage.toString(),
-  };
+export async function fetchContacts(page = 1, perPage = 50) {
+  try {
+    const params = {
+      page: page.toString(),
+      perPage: perPage.toString(),
+    };
 
-  const queryParams = new URLSearchParams(params);
-
-  const response = await axiosInstance.get(
-    `contacts?${queryParams.toString()}`
-  );
-  return response.data;
+    const queryParams = new URLSearchParams(params);
+    const response = await axiosInstance.get(`contacts?${queryParams.toString()}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching contacts:", error);
+    throw error;
+  }
 }
 
 export async function fetchContactById(contactId) {
-  const response = await axiosInstance.get(`contacts/${contactId}`);
-  return response.data;
+  try {
+    const response = await axiosInstance.get(`contacts/${contactId}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching contact with ID ${contactId}:`, error);
+    throw error;
+  }
 }
 
 export async function updateContact(contactId, data) {
-  const response = await axiosInstance.put(`contacts/${contactId}`, data);
-  return response.data;
-} 
+  try {
+    const response = await axiosInstance.put(`contacts/${contactId}`, data);
+    return response.data;
+  } catch (error) {
+    console.error(`Error updating contact with ID ${contactId}:`, error);
+    throw error;
+  }
+}
 
 export async function createContact(data) {
-  const response = await axiosInstance.post(`contacts`, data);
-  return response.data;
+  try {
+    const response = await axiosInstance.post(`contacts`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    throw error;
+  }
 }

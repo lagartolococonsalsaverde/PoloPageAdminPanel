e31d1b1dc2f80dae989520c935e09115a7e30c78
HumayyunKhan <humayyunatwork@gmail.com>
2025-12-23 00:07:32 +0500

    api removal

--- src/services/qna.api.js
+++ src/services/qna.api.js
@@ -1,15 +1,13 @@
-import { BASE_URL } from "../environments/development.environment";
-import { createHttpService } from "../services/httpService";
 import { axiosInstance } from "./dashboard.api";
 
 
 export async function deleteQuestion(questionId) {
-    const response = await axiosInstance.deleteMethod(`api/questions/${questionId.toString()}`);
+    const response = await axiosInstance.deleteMethod(`questions/${questionId.toString()}`);
     return response.data;
 }
 
 export async function deleteAnswer(questionId, answerCreatedTime) {
 
-    const response = await axiosInstance.deleteMethod(`api/questions/${questionId.toString()}/answers?createdAt=${answerCreatedTime}`);
+    const response = await axiosInstance.deleteMethod(`questions/${questionId.toString()}/answers?createdAt=${answerCreatedTime}`);
     return response.data;
 }
